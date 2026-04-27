1f45e8b735b3b97133c7da8105da69f987e20007
Aradhya Nautiyal <134077675+ARADHYA299@users.noreply.github.com>
2026-04-28 00:28:45 +0530

    Add files via upload

--- SynthesisService.py
+++ SynthesisService.py
@@ -2,21 +2,18 @@ class synthesisService:
   
   def __init__(self , llm):
     self.llm = llm
-    
-    def synthesize(self , agentOutputs):
-      
-      prompt = f"""
-      
-      You are a moderator.
-      
-      combine these insights:
-      {agentOutputs}
-      
-      Give:
-        - key issues
-        - quick fixes
-        - strategic improvements
-      """
-      
-      
-      return self.llm(prompt)
+
+  def synthesize(self, agentOutputs):
+    prompt = f"""
+    You are a moderator.
+
+    combine these insights:
+    {agentOutputs}
+
+    Give:
+      - key issues
+      - quick fixes
+      - strategic improvements
+    """
+
+    return self.llm(prompt)
